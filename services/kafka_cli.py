f184b0563eb37c8f458fd07f394fa0413a6bdca0
alireza <alirezashateri7@gmail.com>
2021-04-29 23:59:10 +0430

    Move Topic class to settings file

--- services/kafka_cli.py
+++ services/kafka_cli.py
@@ -1,6 +1,9 @@
 import enum
 import os
 from json import dumps
+
+from django.conf import settings
+
 from kafka import KafkaProducer
 from gateway.settings import KAFKA_ENDPOINT
 from gateway.settings import (
@@ -11,24 +14,9 @@ from gateway.settings import (
 )
 
 kafka_producer = KafkaProducer(
-    bootstrap_servers=KAFKA_ENDPOINT, value_serializer=lambda x: dumps(x).encode('utf-8')
+    bootstrap_servers=KAFKA_ENDPOINT,
+    value_serializer=lambda x: dumps(x).encode('utf-8')
 )
-
-
-class Topic:
-    def __init__(self, topic_name, total_partitions):
-        self.total_partitions = total_partitions
-        self.cur = 0
-        self.topic_name = topic_name
-
-    def get_partition(self):
-        self.cur = (self.cur + 1) % self.total_partitions
-        return self.cur
-
-
-arenas = [Topic(KAFKA_TOPIC_MATCH_0, KAFKA_TOPIC_MATCH_0_PARTITIONS),
-          Topic(KAFKA_TOPIC_MATCH_1, KAFKA_TOPIC_MATCH_1_PARTITIONS),
-          ]
 
 
 class Topics(enum.Enum):
@@ -43,11 +31,12 @@ class KafkaClient:
     @staticmethod
     def register_match(priority, message) -> bool:
         try:
-            arena = arenas[priority]
+            arena = settings.arenas[priority]
             print(arena)
             # kafka_producer.send(topic=arena.topic_name, value=message)
             logging.warning(f"{arena}")
-            kafka_producer.send(topic=arena.topic_name, value=message, partition=arena.get_partition())
+            kafka_producer.send(topic=arena.topic_name, value=message,
+                                partition=arena.get_partition())
             kafka_producer.flush()
             return True
         except Exception as e:
