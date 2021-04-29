055aba5fa88664a993a3e243f8e61370b1d23847
ArshiAAkhavan <letmemakenewone@gmail.com>
2021-04-29 15:21:02 +0430

    partitioner

--- services/kafka_cli.py
+++ services/kafka_cli.py
@@ -3,10 +3,32 @@ import os
 from json import dumps
 from kafka import KafkaProducer
 from gateway.settings import KAFKA_ENDPOINT
+from gateway.settings import (
+    KAFKA_TOPIC_MATCH_0,
+    KAFKA_TOPIC_MATCH_1,
+    KAFKA_TOPIC_MATCH_0_PARTITIONS,
+    KAFKA_TOPIC_MATCH_1_PARTITIONS
+)
 
 kafka_producer = KafkaProducer(
     bootstrap_servers=KAFKA_ENDPOINT, value_serializer=lambda x: dumps(x).encode('utf-8')
 )
+
+
+class Topic:
+    def __init__(self, topic_name, total_partitions):
+        self.total_partitions = total_partitions
+        self.cur = 0
+        self.topic_name = topic_name
+
+    def get_partition(self):
+        self.cur = (self.cur + 1) % self.total_partitions
+        return self.cur
+
+
+arenas = [Topic(KAFKA_TOPIC_MATCH_0, KAFKA_TOPIC_MATCH_0_PARTITIONS),
+          Topic(KAFKA_TOPIC_MATCH_1, KAFKA_TOPIC_MATCH_1_PARTITIONS),
+          ]
 
 
 class Topics(enum.Enum):
@@ -16,7 +38,18 @@ class Topics(enum.Enum):
 
 class KafkaClient:
     @staticmethod
-    def send_message(topic, message) -> bool:
+    def register_match(priority, message) -> bool:
+        try:
+            arena = arenas[priority]
+            kafka_producer.send(topic=arena.topic_name, value=message, partition=arena.get_partition())
+            kafka_producer.flush()
+            return True
+        except Exception as e:
+            print(e)
+            return False
+
+    @staticmethod
+    def send_code(topic, message) -> bool:
         try:
             kafka_producer.send(topic=topic, value=message)
             kafka_producer.flush()
