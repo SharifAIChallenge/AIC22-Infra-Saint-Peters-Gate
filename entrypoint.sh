c19bc0e2afbdb5f74f56fa8acbaac6bfa9fb137c
shanlialefkhani <shanli.alefkhani@gmail.com>
2022-07-20 17:59:05 +0430

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate sites
 python manage.py migrate
-gunicorn --bind=0.0.0.0:5050 --timeout=90
+gunicorn --bind=0.0.0.0:5050 --timeout=90 infra_gateway.wsgi:application
 
