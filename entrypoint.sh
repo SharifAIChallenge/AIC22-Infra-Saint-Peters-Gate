ed4742fd391543b7798cd43fcf44401372ce15dc
shanlialefkhani <shanli.alefkhani@gmail.com>
2022-07-20 18:04:03 +0430

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py migrate sites
 python manage.py migrate
-gunicorn --bind=0.0.0.0:5050 --timeout=90 AIC21-infra-saint_peters_gate.wsgi:application
+gunicorn --bind=0.0.0.0:5050 --timeout=90 gateway.wsgi:application
