f05298d2daf08a6bf653b5b4a9bdc82dae58768a
shanlialefkhani <shanli.alefkhani@gmail.com>
2022-07-24 18:03:28 +0430

    Add logs

--- apps/permissions.py
+++ apps/permissions.py
@@ -6,4 +6,5 @@ from gateway import settings
 class IsBackend(BasePermission):
 
     def has_permission(self, request, view):
+        print("loooog", request.headers.get('Authorization'), len(settings.BACKEND_TOKEN))
         return request.headers.get('Authorization') == settings.BACKEND_TOKEN
